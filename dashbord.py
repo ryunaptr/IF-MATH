44d4daa2989418e1c3c7c02b7bdf6c927c7f2133
nianaa24 <123805309+nianaa24@users.noreply.github.com>
2024-02-13 05:17:01 +0700

    Update dashbord.py
    Signed-off-by: nianaa24 <123805309+nianaa24@users.noreply.github.com>

--- dashbord.py
+++ dashbord.py
@@ -17,7 +17,29 @@ def Analisis_Pelanggan_Unik_Per_Kota(df_customer) :
     
     Lima_Terendah = count_review_city.head()
 
-    st.header("Grafik Ketepatan Jasa Pengiriman")
+    t.header("Grafik 5 Cabang Terendah")
+    st.dataframe(Lima_Terendah)
+
+    # Buat bar chart
+    label = Lima_Terendah['Seller_City']
+    data = Lima_Terendah['Jumlah']
+
+    fig, ax = plt.subplots()
+    ax.bar(label, data, color=['skyblue' if jumlah <= 999 else 'red' for jumlah in data])
+    ax.set_xlabel('Seller_City')
+    ax.set_ylabel('Jumlah')
+
+    #Menambahkan Label Pada Setiap Bar
+    for i in range (len(label)) :
+        ax.text(label[i], data[i], str(data[i]), ha='center', va='bottom' )
+
+    #Rotasi Label 45 derajat
+    plt.xticks(rotation=45)                    
+    st.pyplot(fig)
+
+    #Expander Grafik
+    with st.expander("Penjelasan Cabang Terendah") :
+        st.write('Analisis selanjutnya untuk menambah wawasan pengguna
 
 
 df_customer = load_data("https://raw.githubusercontent.com/nianaa24/IF-MATH/main/customers_dataset.csv")
