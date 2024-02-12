d12ba5c53bc39c452f12027553aabcd04ae354aa
nianaa24 <123805309+nianaa24@users.noreply.github.com>
2024-02-13 04:37:35 +0700

    dashbord.py
    Signed-off-by: nianaa24 <123805309+nianaa24@users.noreply.github.com>

--- dashbord.py
+++ dashbord.py
@@ -12,12 +12,17 @@ def load_data(url) :
     return df
 
 def Analisis_Pelanggan_Unik_Per_Kota(df_customer) :
-    ()
+    count_review_city = df_Review_1['customer_city'].value_counts().reset_index()
+    count_review_city.columns = ['Seller_City','Jumlah']
+    Lima_Terendah = count_review_city.head()
 
+    #Analisis Data Review Penilaian
+    st.header("Grafik Review Terhadap Order Konsumen")
+    count_reviews_sorted = count_reviews.sort_values(by='Review_Score')
+        
+    st.dataframe(count_reviews_sorted)
 
-
-
-#df_customer = load_data("https://github.com/nianaa24/IF-MATH/blob/main/customers_dataset.csv")
+df_customer = load_data("https://github.com/nianaa24/IF-MATH/blob/main/customers_dataset.csv")
 
 with st.sidebar :
     selected = option_menu('Menu',['Dashboard'],
